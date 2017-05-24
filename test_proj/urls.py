0714fac82f290fd954b58dfec599b8665acb4392
Tim Valenta <tonightslastsong@gmail.com>
2017-05-24 11:12:33 -0800

    Update urlconf code for django 1.10

--- test_proj/urls.py
+++ test_proj/urls.py
@@ -1,13 +1,12 @@
-from .compat import patterns, include, url
-
+from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'test_proj.views.home', name='home'),
     # url(r'^test_proj/', include('test_proj.foo.urls')),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', admin.site.urls),
-)
+]
